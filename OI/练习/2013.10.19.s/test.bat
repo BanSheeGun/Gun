7a47d4fc6c61f3b2a31e3b8045ebead09dffc6c3
Gunpowder <ccp750707@126.com>
2015-10-29 20:26:49 +0800

    OI_3

--- OI/练习/2013.10.19.s/test.bat
+++
@@ -1,9 +0,0 @@
-:loop
- ran.exe
- 2_1.exe
- time<enter
- 2.exe
- time<enter
- fc 0.out 2.out
- pause
-goto :loop
--- OI/练习/2013.11.2.s/2.bat
+++
@@ -1,8 +0,0 @@
-:loop
- test.exe
- hotel.exe
- hotel_1.exe
- fc hotel.out hotel1.out
- pause
- cls
-goto :loop
